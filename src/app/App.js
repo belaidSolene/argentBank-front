df1b98c20b0a27a2975524f2f580ba6cfc0a2880
Belaid Solene <solene.belaid@greta-cfa-aquitaine.academy>
2023-12-11 14:34:45 +0100

    init store

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,27 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-          Learn React
-      </header>
-    </div>
-  );
+/* import App from './app/App'; */
+import Index from '../pages/Index';
+import SignIn from '../pages/SignIn';
+import User from '../pages/User';
+
+// Creating a browser router with defined routes and associated components
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Index />,
+	},
+  {
+		path: '/login',
+		element: <SignIn />,
+	},
+  {
+		path: '/profil',
+		element: <User />,
+	},
+])
+
+export default function App() {
+  return <RouterProvider router={router} />
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
+
+import store from './app/store'
+
+import App from './app/App';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-/* import App from './app/App'; */
-import Index from './pages/Index';
-import SignIn from './pages/SignIn';
-import User from './pages/User';
-
-// Creating a browser router with defined routes and associated components
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Index />,
-	},
-  {
-		path: '/login',
-		element: <SignIn />,
-	},
-  {
-		path: '/profil',
-		element: <User />,
-	},
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 )
